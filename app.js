41b05160b5916cca972658534eae9844ff988496
Kruthika Suresh Ved <kruthika.ved@gmail.com>
2017-10-07 18:05:38 +0200

    api integrated with mongo

--- app.js
+++ app.js
@@ -29,7 +29,8 @@ steps.remove();
 var img1 = "./sample_data/Rattigheter.PNG";
 var base64_img1 = base64_encode(img1);
 var base64_snd1 = base64_encode('./sample_data/rattigheter.m4a');
-var id1 = new ObjectID();
+//var id1 = new ObjectID();
+var id1 = 1;
 steps.insert({
 	_id:  id1,
 	description: "Rattigheter",			
@@ -42,7 +43,8 @@ steps.insert({
 //Create Step2 document
 var base64_img2 = base64_encode("./sample_data/Att_vanta_pa_svar_red.png");
 var base64_snd2 = base64_encode('./sample_data/Att_vanta_pa_svar.m4a');
-var id2 = new ObjectID();
+//var id2 = new ObjectID();
+var id2 = 2;
 steps.insert({
 	_id: id2,
 	description: "Att vänta på svar red",			
@@ -54,7 +56,8 @@ steps.insert({
 //Create Step4 document
 var base64_img4 = base64_encode("./sample_data/att_vara_asylsokande_red.png");
 var base64_snd4 = base64_encode('./sample_data/Att_vara_asylsokande.m4a');
-var id4 = new ObjectID();
+//var id4 = new ObjectID();
+var id4 = 4;
 steps.insert({
 	_id: id3,
 	description: "Att vara asylsokande",			
@@ -67,7 +70,8 @@ steps.insert({
 //Create Step3 document
 var base64_img3 = base64_encode("./sample_data/fingeravtryck_red.png");
 var base64_snd3 = base64_encode('./sample_data/Fingeravtryck.m4a');
-var id3 = new ObjectID();
+//var id3 = new ObjectID();
+var id3 = 3;
 steps.insert({
 	_id:  id3,
 	description: "fingeravtryck",			
@@ -110,17 +114,17 @@ processCollection.insert({
 
 //Mobile and Admin
 app.get('/getProcesses', (req, res) => {
-    var testJson = [
-        {
-            id: 1,
-            name: 'firstProcess',
-            steps: [2, 1, 3],
-            version: 1
-        }
-    ];
-
-    res.send(testJson);
+    processCollection.find({}, function(err, result) {
+	   if (err) {
+		   // If it failed, return error
+		   res.send("There was an error finding user");
+	   }
+	   else {
+		   res.send(result); // The result should be a JSON object that you can access with result.field (example)
+	   }
+	});
 });
+		
 
 //Mobile and admin
 // app.get('/getStep', (req, res) => {
@@ -147,20 +151,25 @@ app.get('/login', (req, res) => {
 
 //Admin endpoint
 app.post('/updateProcess', (req, res) => {
-    console.log('updateProcess', req);
+	req.body.version += 1;
+	console.log(req.body);
+	processCollection.update({_id: ObjectID(req.body._id)}, req.body);
+	
     //Update process in database
-    res.sendStatus(200);
+    res.send(JSON.stringify(req.body));
 })
 
 //Admin endpoint
 app.post('/updateStep', (req, res) => {
     console.log('Update step', req.body);
     //Update step
-    res.sendStatus(200);
+	req.body.version += 1;
+	steps.update({_id: ObjectID(req.body._id)}, req.body);
+    res.send(JSON.stringify(req.body));
 });
 
 app.post('/deleteStep', (req, res) => {
-
+	steps.deleteOne({_id: ObjectID(req.body._id)});
 });
 
 app.listen(8080, () => {
